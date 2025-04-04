fb00fba00d40b467838c117ecd5ac76e14789fb2
Gonçalo Branco <goncalo.branco@devscope.net>
2025-04-04 11:53:30 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base : 'gwhitePort',
+  base : '/f-757466/',
   plugins: [
     react(),
     mode === 'development' &&
